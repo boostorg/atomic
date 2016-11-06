07be21c7e839adaa0bb84859933b2dab06d631ce
Andrey Semashev <andrey.semashev@gmail.com>
2016-11-06 21:29:33 +0300

    Silenced some MSVC warnings.

--- include/boost/atomic/detail/ops_msvc_x86.hpp
+++ include/boost/atomic/detail/ops_msvc_x86.hpp
@@ -772,7 +772,7 @@ struct msvc_dcas_x86
         return compare_exchange_strong(storage, expected, desired, success_order, failure_order);
     }
 
-    static BOOST_FORCEINLINE storage_type exchange(storage_type volatile& storage, storage_type v, memory_order order) BOOST_NOEXCEPT
+    static BOOST_FORCEINLINE storage_type exchange(storage_type volatile& storage, storage_type v, memory_order) BOOST_NOEXCEPT
     {
         BOOST_ATOMIC_DETAIL_COMPILER_BARRIER();
 
