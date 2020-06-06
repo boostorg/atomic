e4f8770665f3c91c3ad8d53d7384cb5f2ca97985
Andrey Semashev <andrey.semashev@gmail.com>
2020-06-06 19:10:01 +0300

    Reorganized atomic, atomic_ref and atomic_flag implementation.
    Moved public classes definitions to the public headers and renamed
    the internal implementation headers. This will allow to reuse the
    implementation headers for inter-process atomics later.

--- include/boost/atomic/detail/atomic_impl.hpp
+++ include/boost/atomic/detail/atomic_impl.hpp
@@ -8,22 +8,19 @@
  * Copyright (c) 2014-2020 Andrey Semashev
  */
 /*!
- * \file   atomic/detail/atomic_template.hpp
+ * \file   atomic/detail/atomic_impl.hpp
  *
- * This header contains interface definition of \c atomic template.
+ * This header contains implementation of \c atomic template.
  */
 
-#ifndef BOOST_ATOMIC_DETAIL_ATOMIC_TEMPLATE_HPP_INCLUDED_
-#define BOOST_ATOMIC_DETAIL_ATOMIC_TEMPLATE_HPP_INCLUDED_
+#ifndef BOOST_ATOMIC_DETAIL_ATOMIC_IMPL_HPP_INCLUDED_
+#define BOOST_ATOMIC_DETAIL_ATOMIC_IMPL_HPP_INCLUDED_
 
 #include <cstddef>
-#include <boost/cstdint.hpp>
 #include <boost/assert.hpp>
-#include <boost/static_assert.hpp>
 #include <boost/memory_order.hpp>
 #include <boost/atomic/detail/config.hpp>
 #include <boost/atomic/detail/intptr.hpp>
-#include <boost/atomic/detail/classify.hpp>
 #include <boost/atomic/detail/storage_traits.hpp>
 #include <boost/atomic/detail/bitwise_cast.hpp>
 #include <boost/atomic/detail/integral_conversions.hpp>
@@ -33,7 +30,6 @@
 #include <boost/atomic/detail/memory_order_utils.hpp>
 #include <boost/atomic/detail/aligned_variable.hpp>
 #include <boost/atomic/detail/type_traits/is_signed.hpp>
-#include <boost/atomic/detail/type_traits/is_trivially_copyable.hpp>
 #include <boost/atomic/detail/type_traits/is_trivially_default_constructible.hpp>
 #include <boost/atomic/detail/type_traits/alignment_of.hpp>
 #include <boost/atomic/detail/type_traits/conditional.hpp>
@@ -1139,129 +1135,6 @@ private:
 };
 
 } // namespace detail
-
-template< typename T >
-class atomic :
-    public atomics::detail::base_atomic< T, typename atomics::detail::classify< T >::type >
-{
-private:
-    typedef atomics::detail::base_atomic< T, typename atomics::detail::classify< T >::type > base_type;
-    typedef typename base_type::value_arg_type value_arg_type;
-
-public:
-    typedef typename base_type::value_type value_type;
-    // Deprecated, use value_type instead
-    BOOST_ATOMIC_DETAIL_STORAGE_DEPRECATED
-    typedef typename base_type::storage_type storage_type;
-
-    BOOST_STATIC_ASSERT_MSG(sizeof(value_type) > 0u, "boost::atomic<T> requires T to be a complete type");
-#if !defined(BOOST_ATOMIC_DETAIL_NO_CXX11_IS_TRIVIALLY_COPYABLE)
-    BOOST_STATIC_ASSERT_MSG(atomics::detail::is_trivially_copyable< value_type >::value, "boost::atomic<T> requires T to be a trivially copyable type");
-#endif
-
-public:
-    BOOST_DEFAULTED_FUNCTION(atomic() BOOST_ATOMIC_DETAIL_DEF_NOEXCEPT_DECL, BOOST_ATOMIC_DETAIL_DEF_NOEXCEPT_IMPL {})
-    BOOST_FORCEINLINE BOOST_ATOMIC_DETAIL_CONSTEXPR_UNION_INIT atomic(value_arg_type v) BOOST_NOEXCEPT : base_type(v) {}
-
-    BOOST_FORCEINLINE value_type operator= (value_arg_type v) BOOST_NOEXCEPT
-    {
-        this->store(v);
-        return v;
-    }
-
-    BOOST_FORCEINLINE value_type operator= (value_arg_type v) volatile BOOST_NOEXCEPT
-    {
-        this->store(v);
-        return v;
-    }
-
-    BOOST_FORCEINLINE operator value_type() const volatile BOOST_NOEXCEPT
-    {
-        return this->load();
-    }
-
-    // Deprecated, use value() instead
-    BOOST_ATOMIC_DETAIL_STORAGE_DEPRECATED
-    BOOST_FORCEINLINE typename base_type::storage_type& storage() BOOST_NOEXCEPT { return base_type::storage(); }
-    BOOST_ATOMIC_DETAIL_STORAGE_DEPRECATED
-    BOOST_FORCEINLINE typename base_type::storage_type volatile& storage() volatile BOOST_NOEXCEPT { return base_type::storage(); }
-    BOOST_ATOMIC_DETAIL_STORAGE_DEPRECATED
-    BOOST_FORCEINLINE typename base_type::storage_type const& storage() const BOOST_NOEXCEPT { return base_type::storage(); }
-    BOOST_ATOMIC_DETAIL_STORAGE_DEPRECATED
-    BOOST_FORCEINLINE typename base_type::storage_type const volatile& storage() const volatile BOOST_NOEXCEPT { return base_type::storage(); }
-
-    BOOST_DELETED_FUNCTION(atomic(atomic const&))
-    BOOST_DELETED_FUNCTION(atomic& operator= (atomic const&))
-    BOOST_DELETED_FUNCTION(atomic& operator= (atomic const&) volatile)
-};
-
-typedef atomic< char > atomic_char;
-typedef atomic< unsigned char > atomic_uchar;
-typedef atomic< signed char > atomic_schar;
-typedef atomic< uint8_t > atomic_uint8_t;
-typedef atomic< int8_t > atomic_int8_t;
-typedef atomic< unsigned short > atomic_ushort;
-typedef atomic< short > atomic_short;
-typedef atomic< uint16_t > atomic_uint16_t;
-typedef atomic< int16_t > atomic_int16_t;
-typedef atomic< unsigned int > atomic_uint;
-typedef atomic< int > atomic_int;
-typedef atomic< uint32_t > atomic_uint32_t;
-typedef atomic< int32_t > atomic_int32_t;
-typedef atomic< unsigned long > atomic_ulong;
-typedef atomic< long > atomic_long;
-typedef atomic< uint64_t > atomic_uint64_t;
-typedef atomic< int64_t > atomic_int64_t;
-#ifdef BOOST_HAS_LONG_LONG
-typedef atomic< boost::ulong_long_type > atomic_ullong;
-typedef atomic< boost::long_long_type > atomic_llong;
-#endif
-typedef atomic< void* > atomic_address;
-typedef atomic< bool > atomic_bool;
-typedef atomic< wchar_t > atomic_wchar_t;
-#if defined(__cpp_char8_t) && __cpp_char8_t >= 201811
-typedef atomic< char8_t > atomic_char8_t;
-#endif
-#if !defined(BOOST_NO_CXX11_CHAR16_T)
-typedef atomic< char16_t > atomic_char16_t;
-#endif
-#if !defined(BOOST_NO_CXX11_CHAR32_T)
-typedef atomic< char32_t > atomic_char32_t;
-#endif
-
-typedef atomic< int_least8_t > atomic_int_least8_t;
-typedef atomic< uint_least8_t > atomic_uint_least8_t;
-typedef atomic< int_least16_t > atomic_int_least16_t;
-typedef atomic< uint_least16_t > atomic_uint_least16_t;
-typedef atomic< int_least32_t > atomic_int_least32_t;
-typedef atomic< uint_least32_t > atomic_uint_least32_t;
-typedef atomic< int_least64_t > atomic_int_least64_t;
-typedef atomic< uint_least64_t > atomic_uint_least64_t;
-typedef atomic< int_fast8_t > atomic_int_fast8_t;
-typedef atomic< uint_fast8_t > atomic_uint_fast8_t;
-typedef atomic< int_fast16_t > atomic_int_fast16_t;
-typedef atomic< uint_fast16_t > atomic_uint_fast16_t;
-typedef atomic< int_fast32_t > atomic_int_fast32_t;
-typedef atomic< uint_fast32_t > atomic_uint_fast32_t;
-typedef atomic< int_fast64_t > atomic_int_fast64_t;
-typedef atomic< uint_fast64_t > atomic_uint_fast64_t;
-typedef atomic< intmax_t > atomic_intmax_t;
-typedef atomic< uintmax_t > atomic_uintmax_t;
-
-#if !defined(BOOST_ATOMIC_NO_FLOATING_POINT)
-typedef atomic< float > atomic_float_t;
-typedef atomic< double > atomic_double_t;
-typedef atomic< long double > atomic_long_double_t;
-#endif
-
-typedef atomic< std::size_t > atomic_size_t;
-typedef atomic< std::ptrdiff_t > atomic_ptrdiff_t;
-
-#if defined(BOOST_HAS_INTPTR_T)
-typedef atomic< boost::intptr_t > atomic_intptr_t;
-typedef atomic< boost::uintptr_t > atomic_uintptr_t;
-#endif
-
 } // namespace atomics
 } // namespace boost
 
@@ -1269,4 +1142,4 @@ typedef atomic< boost::uintptr_t > atomic_uintptr_t;
 #pragma warning(pop)
 #endif
 
-#endif // BOOST_ATOMIC_DETAIL_ATOMIC_TEMPLATE_HPP_INCLUDED_
+#endif // BOOST_ATOMIC_DETAIL_ATOMIC_IMPl_HPP_INCLUDED_
--- include/boost/atomic/detail/atomic_ref_impl.hpp
+++ include/boost/atomic/detail/atomic_ref_impl.hpp
@@ -6,22 +6,18 @@
  * Copyright (c) 2020 Andrey Semashev
  */
 /*!
- * \file   atomic/detail/atomic_ref_template.hpp
+ * \file   atomic/detail/atomic_ref_impl.hpp
  *
- * This header contains interface definition of \c atomic_ref template.
+ * This header contains implementation of \c atomic_ref template.
  */
 
-#ifndef BOOST_ATOMIC_DETAIL_ATOMIC_REF_TEMPLATE_HPP_INCLUDED_
-#define BOOST_ATOMIC_DETAIL_ATOMIC_REF_TEMPLATE_HPP_INCLUDED_
+#ifndef BOOST_ATOMIC_DETAIL_ATOMIC_REF_IMPL_HPP_INCLUDED_
+#define BOOST_ATOMIC_DETAIL_ATOMIC_REF_IMPL_HPP_INCLUDED_
 
 #include <cstddef>
-#include <boost/cstdint.hpp>
 #include <boost/assert.hpp>
-#include <boost/static_assert.hpp>
 #include <boost/memory_order.hpp>
 #include <boost/atomic/detail/config.hpp>
-#include <boost/atomic/detail/intptr.hpp>
-#include <boost/atomic/detail/classify.hpp>
 #include <boost/atomic/detail/addressof.hpp>
 #include <boost/atomic/detail/storage_traits.hpp>
 #include <boost/atomic/detail/bitwise_cast.hpp>
@@ -30,8 +26,6 @@
 #include <boost/atomic/detail/extra_operations.hpp>
 #include <boost/atomic/detail/memory_order_utils.hpp>
 #include <boost/atomic/detail/type_traits/is_signed.hpp>
-#include <boost/atomic/detail/type_traits/is_trivially_copyable.hpp>
-#include <boost/atomic/detail/type_traits/is_trivially_default_constructible.hpp>
 #include <boost/atomic/detail/type_traits/alignment_of.hpp>
 #include <boost/atomic/detail/type_traits/conditional.hpp>
 #include <boost/atomic/detail/type_traits/integral_constant.hpp>
@@ -1121,48 +1115,6 @@ private:
 };
 
 } // namespace detail
-
-template< typename T >
-class atomic_ref :
-    public atomics::detail::base_atomic_ref< T, typename atomics::detail::classify< T >::type >
-{
-private:
-    typedef atomics::detail::base_atomic_ref< T, typename atomics::detail::classify< T >::type > base_type;
-    typedef typename base_type::value_arg_type value_arg_type;
-
-public:
-    typedef typename base_type::value_type value_type;
-
-    BOOST_STATIC_ASSERT_MSG(sizeof(value_type) > 0u, "boost::atomic_ref<T> requires T to be a complete type");
-#if !defined(BOOST_ATOMIC_DETAIL_NO_CXX11_IS_TRIVIALLY_COPYABLE)
-    BOOST_STATIC_ASSERT_MSG(atomics::detail::is_trivially_copyable< value_type >::value, "boost::atomic_ref<T> requires T to be a trivially copyable type");
-#endif
-
-private:
-    typedef typename base_type::storage_type storage_type;
-
-public:
-    BOOST_DEFAULTED_FUNCTION(atomic_ref(atomic_ref const& that) BOOST_ATOMIC_DETAIL_DEF_NOEXCEPT_DECL, BOOST_ATOMIC_DETAIL_DEF_NOEXCEPT_IMPL : base_type(static_cast< base_type const& >(that)) {})
-    BOOST_FORCEINLINE explicit atomic_ref(value_type& v) BOOST_NOEXCEPT : base_type(v)
-    {
-        // Check that referenced object alignment satisfies required alignment
-        BOOST_ASSERT((((atomics::detail::uintptr_t)this->m_value) & (base_type::required_alignment - 1u)) == 0u);
-    }
-
-    BOOST_FORCEINLINE value_type operator= (value_arg_type v) const BOOST_NOEXCEPT
-    {
-        this->store(v);
-        return v;
-    }
-
-    BOOST_FORCEINLINE operator value_type() const BOOST_NOEXCEPT
-    {
-        return this->load();
-    }
-
-    BOOST_DELETED_FUNCTION(atomic_ref& operator= (atomic_ref const&))
-};
-
 } // namespace atomics
 } // namespace boost
 
@@ -1170,4 +1122,4 @@ public:
 #pragma warning(pop)
 #endif
 
-#endif // BOOST_ATOMIC_DETAIL_ATOMIC_REF_TEMPLATE_HPP_INCLUDED_
+#endif // BOOST_ATOMIC_DETAIL_ATOMIC_REF_IMPL_HPP_INCLUDED_
