411f6f65ab7b4281031cc6dfa4d34989ba5b76b0
Andrey Semashev <andrey.semashev@gmail.com>
2025-05-29 01:49:47 +0300

    Removed support for Windows versions prior to Windows 10.
    This makes WaitOnAddress/WakeByAddressSingle/WakeByAddressAll a mandatory
    requirement. Thus, runtime detection of these APIs is now removed.
    
    Also, linking with synchronization lib on Windows is now mandatory. The
    library provides WaitOnAddress & co. which are now mandatory.

--- include/boost/atomic/detail/wait_ops_windows.hpp
+++ include/boost/atomic/detail/wait_ops_windows.hpp
@@ -3,7 +3,7 @@
  * (See accompanying file LICENSE_1_0.txt or copy at
  * http://www.boost.org/LICENSE_1_0.txt)
  *
- * Copyright (c) 2020 Andrey Semashev
+ * Copyright (c) 2020-2025 Andrey Semashev
  */
 /*!
  * \file   atomic/detail/wait_ops_windows.hpp
@@ -14,14 +14,13 @@
 #ifndef BOOST_ATOMIC_DETAIL_WAIT_OPS_WINDOWS_HPP_INCLUDED_
 #define BOOST_ATOMIC_DETAIL_WAIT_OPS_WINDOWS_HPP_INCLUDED_
 
-#include <boost/atomic/detail/config.hpp>
 #include <boost/memory_order.hpp>
-#include <boost/winapi/wait_constants.hpp>
+#include <boost/atomic/detail/config.hpp>
 #include <boost/atomic/detail/wait_operations_fwd.hpp>
 #include <boost/atomic/detail/wait_capabilities.hpp>
-#if defined(BOOST_ATOMIC_DETAIL_WINDOWS_HAS_WAIT_ON_ADDRESS)
+#include <boost/winapi/wait_constants.hpp>
 #include <boost/winapi/wait_on_address.hpp>
-#if (defined(BOOST_ATOMIC_FORCE_AUTO_LINK) || (!defined(BOOST_ALL_NO_LIB) && !defined(BOOST_ATOMIC_NO_LIB))) && !defined(BOOST_ATOMIC_NO_SYNCHRONIZATION_LIB)
+#if (defined(BOOST_ATOMIC_FORCE_AUTO_LINK) || (!defined(BOOST_ALL_NO_LIB) && !defined(BOOST_ATOMIC_NO_LIB)))
 #define BOOST_LIB_NAME "synchronization"
 #if defined(BOOST_AUTO_LINK_NOMANGLE)
 #include <boost/config/auto_link.hpp>
@@ -30,12 +29,7 @@
 #include <boost/config/auto_link.hpp>
 #undef BOOST_AUTO_LINK_NOMANGLE
 #endif // defined(BOOST_AUTO_LINK_NOMANGLE)
-#endif // (defined(BOOST_ATOMIC_FORCE_AUTO_LINK) || (!defined(BOOST_ALL_NO_LIB) && !defined(BOOST_ATOMIC_NO_LIB))) && !defined(BOOST_ATOMIC_NO_SYNCHRONIZATION_LIB)
-#else // defined(BOOST_ATOMIC_DETAIL_WINDOWS_HAS_WAIT_ON_ADDRESS)
-#include <cstddef>
-#include <boost/atomic/detail/wait_on_address.hpp>
-#include <boost/atomic/detail/wait_ops_generic.hpp>
-#endif // defined(BOOST_ATOMIC_DETAIL_WINDOWS_HAS_WAIT_ON_ADDRESS)
+#endif // (defined(BOOST_ATOMIC_FORCE_AUTO_LINK) || (!defined(BOOST_ALL_NO_LIB) && !defined(BOOST_ATOMIC_NO_LIB)))
 #include <boost/atomic/detail/header.hpp>
 
 #ifdef BOOST_HAS_PRAGMA_ONCE
@@ -45,8 +39,6 @@
 namespace boost {
 namespace atomics {
 namespace detail {
-
-#if defined(BOOST_ATOMIC_DETAIL_WINDOWS_HAS_WAIT_ON_ADDRESS)
 
 template< typename Base, std::size_t Size >
 struct wait_operations_windows :
@@ -84,67 +76,6 @@ struct wait_operations_windows :
         boost::winapi::WakeByAddressAll(const_cast< storage_type* >(&storage));
     }
 };
-
-#else // defined(BOOST_ATOMIC_DETAIL_WINDOWS_HAS_WAIT_ON_ADDRESS)
-
-template< typename Base, std::size_t Size >
-struct wait_operations_windows :
-    public atomics::detail::wait_operations_generic< Base, false >
-{
-    typedef atomics::detail::wait_operations_generic< Base, false > base_type;
-    typedef typename base_type::storage_type storage_type;
-
-    static BOOST_CONSTEXPR_OR_CONST bool always_has_native_wait_notify = false;
-
-    static BOOST_FORCEINLINE bool has_native_wait_notify(storage_type const volatile&) BOOST_NOEXCEPT
-    {
-        ensure_wait_functions_initialized();
-        return atomics::detail::wait_on_address != NULL;
-    }
-
-    static BOOST_FORCEINLINE storage_type wait(storage_type const volatile& storage, storage_type old_val, memory_order order) BOOST_NOEXCEPT
-    {
-        ensure_wait_functions_initialized();
-
-        if (BOOST_LIKELY(atomics::detail::wait_on_address != NULL))
-        {
-            storage_type new_val = base_type::load(storage, order);
-            while (new_val == old_val)
-            {
-                atomics::detail::wait_on_address(const_cast< storage_type* >(&storage), &old_val, Size, boost::winapi::infinite);
-                new_val = base_type::load(storage, order);
-            }
-
-            return new_val;
-        }
-        else
-        {
-            return base_type::wait(storage, old_val, order);
-        }
-    }
-
-    static BOOST_FORCEINLINE void notify_one(storage_type volatile& storage) BOOST_NOEXCEPT
-    {
-        ensure_wait_functions_initialized();
-
-        if (BOOST_LIKELY(atomics::detail::wake_by_address_single != NULL))
-            atomics::detail::wake_by_address_single(const_cast< storage_type* >(&storage));
-        else
-            base_type::notify_one(storage);
-    }
-
-    static BOOST_FORCEINLINE void notify_all(storage_type volatile& storage) BOOST_NOEXCEPT
-    {
-        ensure_wait_functions_initialized();
-
-        if (BOOST_LIKELY(atomics::detail::wake_by_address_all != NULL))
-            atomics::detail::wake_by_address_all(const_cast< storage_type* >(&storage));
-        else
-            base_type::notify_all(storage);
-    }
-};
-
-#endif // defined(BOOST_ATOMIC_DETAIL_WINDOWS_HAS_WAIT_ON_ADDRESS)
 
 template< typename Base >
 struct wait_operations< Base, 1u, true, false > :
