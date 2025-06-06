3a6abd10c1928a41395db9eb3fdc31f1c82476c2
Andrey Semashev <andrey.semashev@gmail.com>
2025-06-06 03:51:08 +0300

    Added tests for timed waiting operations.

--- test/ipc_wait_test_helpers.hpp
+++ test/ipc_wait_test_helpers.hpp
@@ -9,6 +9,7 @@
 
 #include <boost/memory_order.hpp>
 #include <boost/atomic/ipc_atomic_flag.hpp>
+#include <boost/atomic/wait_result.hpp>
 
 #include <cstdlib>
 #include <cstring>
@@ -19,6 +20,7 @@
 #include <iostream>
 #include <type_traits>
 #include <boost/config.hpp>
+#include <boost/current_function.hpp>
 #include <boost/atomic/capabilities.hpp>
 #include <boost/atomic/ipc_atomic_flag.hpp>
 #include "atomic_wrapper.hpp"
@@ -28,7 +30,7 @@
 #include "test_barrier.hpp"
 
 //! Since some of the tests below are allowed to fail, we retry up to this many times to pass the test
-constexpr unsigned int test_retry_count = 5u;
+constexpr unsigned int test_retry_count = 10u;
 
 //! The test verifies that the wait operation returns immediately if the passed value does not match the atomic value
 template< template< typename > class Wrapper, typename T >
@@ -36,8 +38,30 @@ inline void test_wait_value_mismatch(T value1, T value2)
 {
     Wrapper< T > m_wrapper(value1);
 
-    T received_value = m_wrapper.a.wait(value2);
-    BOOST_TEST(received_value == value1);
+    {
+        T received_value = m_wrapper.a.wait(value2);
+        BOOST_TEST(received_value == value1);
+    }
+    {
+        boost::atomics::wait_result< T > result = m_wrapper.a.wait_until(value2, std::chrono::steady_clock::now());
+        BOOST_TEST(result.value == value1);
+        BOOST_TEST(!result.timeout);
+    }
+    {
+        boost::atomics::wait_result< T > result = m_wrapper.a.wait_until(value2, std::chrono::steady_clock::now() + std::chrono::milliseconds(200));
+        BOOST_TEST(result.value == value1);
+        BOOST_TEST(!result.timeout);
+    }
+    {
+        boost::atomics::wait_result< T > result = m_wrapper.a.wait_for(value2, std::chrono::milliseconds::zero());
+        BOOST_TEST(result.value == value1);
+        BOOST_TEST(!result.timeout);
+    }
+    {
+        boost::atomics::wait_result< T > result = m_wrapper.a.wait_for(value2, std::chrono::milliseconds(200));
+        BOOST_TEST(result.value == value1);
+        BOOST_TEST(!result.timeout);
+    }
 }
 
 /*!
@@ -97,17 +121,17 @@ public:
         m_wrapper.a.store(m_value2, boost::memory_order_release);
         m_wrapper.a.notify_one();
 
-        std::this_thread::sleep_until(start_time + std::chrono::milliseconds(400));
+        std::this_thread::sleep_until(start_time + std::chrono::milliseconds(500));
 
         m_wrapper.a.store(m_value3, boost::memory_order_release);
         m_wrapper.a.notify_one();
 
-        if (!thread1.try_join_for(std::chrono::seconds(3)))
+        if (!thread1.try_join_for(std::chrono::seconds(5)))
         {
             BOOST_ERROR("Thread 1 failed to join");
             std::abort();
         }
-        if (!thread2.try_join_for(std::chrono::seconds(3)))
+        if (!thread2.try_join_for(std::chrono::seconds(5)))
         {
             BOOST_ERROR("Thread 2 failed to join");
             std::abort();
@@ -122,21 +146,21 @@ public:
         {
             if ((first_state->m_wakeup_time - start_time) < std::chrono::milliseconds(200))
             {
-                std::cout << "notify_one_test: first thread woke up too soon: "
+                std::cout << BOOST_CURRENT_FUNCTION << ": first thread woke up too soon: "
                     << std::chrono::duration_cast< std::chrono::milliseconds >(first_state->m_wakeup_time - start_time).count() << " ms" << std::endl;
                 return false;
             }
 
-            if ((first_state->m_wakeup_time - start_time) >= std::chrono::milliseconds(400))
+            if ((first_state->m_wakeup_time - start_time) >= std::chrono::milliseconds(500))
             {
-                std::cout << "notify_one_test: first thread woke up too late: "
+                std::cout << BOOST_CURRENT_FUNCTION << ": first thread woke up too late: "
                     << std::chrono::duration_cast< std::chrono::milliseconds >(first_state->m_wakeup_time - start_time).count() << " ms" << std::endl;
                 return false;
             }
 
-            if ((second_state->m_wakeup_time - start_time) < std::chrono::milliseconds(400))
+            if ((second_state->m_wakeup_time - start_time) < std::chrono::milliseconds(500))
             {
-                std::cout << "notify_one_test: second thread woke up too soon: "
+                std::cout << BOOST_CURRENT_FUNCTION << ": second thread woke up too soon: "
                     << std::chrono::duration_cast< std::chrono::milliseconds >(second_state->m_wakeup_time - start_time).count() << " ms" << std::endl;
                 return false;
             }
@@ -173,6 +197,8 @@ inline void test_notify_one(T value1, T value2, T value3)
         std::unique_ptr< notify_one_test< Wrapper, T > > test(new notify_one_test< Wrapper, T >(value1, value2, value3));
         if (test->run())
             return;
+
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
     }
 
     BOOST_ERROR("notify_one_test could not complete because blocked thread wake up too soon");
@@ -234,12 +260,12 @@ public:
         m_wrapper.a.store(m_value2, boost::memory_order_release);
         m_wrapper.a.notify_all();
 
-        if (!thread1.try_join_for(std::chrono::seconds(3)))
+        if (!thread1.try_join_for(std::chrono::seconds(5)))
         {
             BOOST_ERROR("Thread 1 failed to join");
             std::abort();
         }
-        if (!thread2.try_join_for(std::chrono::seconds(3)))
+        if (!thread2.try_join_for(std::chrono::seconds(5)))
         {
             BOOST_ERROR("Thread 2 failed to join");
             std::abort();
@@ -249,14 +275,14 @@ public:
         {
             if ((m_thread1_state.m_wakeup_time - start_time) < std::chrono::milliseconds(200))
             {
-                std::cout << "notify_all_test: first thread woke up too soon: "
+                std::cout << BOOST_CURRENT_FUNCTION << ": first thread woke up too soon: "
                     << std::chrono::duration_cast< std::chrono::milliseconds >(m_thread1_state.m_wakeup_time - start_time).count() << " ms" << std::endl;
                 return false;
             }
 
             if ((m_thread2_state.m_wakeup_time - start_time) < std::chrono::milliseconds(200))
             {
-                std::cout << "notify_all_test: second thread woke up too soon: "
+                std::cout << BOOST_CURRENT_FUNCTION << ": second thread woke up too soon: "
                     << std::chrono::duration_cast< std::chrono::milliseconds >(m_thread2_state.m_wakeup_time - start_time).count() << " ms" << std::endl;
                 return false;
             }
@@ -287,9 +313,323 @@ inline void test_notify_all(T value1, T value2)
         std::unique_ptr< notify_all_test< Wrapper, T > > test(new notify_all_test< Wrapper, T >(value1, value2));
         if (test->run())
             return;
+
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
     }
 
     BOOST_ERROR("notify_all_test could not complete because blocked thread wake up too soon");
+}
+
+/*!
+ * The test verifies that absolute timeout expiry is correctly registered.
+ */
+template< template< typename > class Wrapper, typename T, typename Clock >
+class abs_timeout_test
+{
+private:
+    Wrapper< T > m_wrapper;
+    T m_value1;
+
+public:
+    explicit abs_timeout_test(T value1) :
+        m_wrapper(value1),
+        m_value1(value1)
+    {
+    }
+
+    bool run()
+    {
+        typename Clock::time_point start_time = Clock::now();
+
+        boost::atomics::wait_result< T > result = m_wrapper.a.wait_until(m_value1, start_time + std::chrono::milliseconds(200));
+
+        typename Clock::time_point wakeup_time = Clock::now();
+
+        if ((wakeup_time - start_time) < std::chrono::milliseconds(200))
+        {
+            std::cout << BOOST_CURRENT_FUNCTION << ": thread woke up too soon: "
+                << std::chrono::duration_cast< std::chrono::milliseconds >(wakeup_time - start_time).count() << " ms" << std::endl;
+            return false;
+        }
+
+        BOOST_TEST_EQ(result.value, m_value1);
+        BOOST_TEST(result.timeout);
+
+        return true;
+    }
+};
+
+template< template< typename > class Wrapper, typename Clock, typename T >
+inline void test_abs_timeout(T value1)
+{
+    for (unsigned int i = 0u; i < test_retry_count; ++i)
+    {
+        // Avoid creating IPC atomics on the stack as this breaks on Darwin
+        std::unique_ptr< abs_timeout_test< Wrapper, T, Clock > > test(new abs_timeout_test< Wrapper, T, Clock >(value1));
+        if (test->run())
+            return;
+
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    }
+
+    BOOST_ERROR("abs_timeout_test could not complete because blocked thread wake up too soon");
+}
+
+/*!
+ * The test verifies that relative timeout expiry is correctly registered.
+ */
+template< template< typename > class Wrapper, typename T >
+class rel_timeout_test
+{
+private:
+    Wrapper< T > m_wrapper;
+    T m_value1;
+
+public:
+    explicit rel_timeout_test(T value1) :
+        m_wrapper(value1),
+        m_value1(value1)
+    {
+    }
+
+    bool run()
+    {
+        test_clock::time_point start_time = test_clock::now();
+
+        boost::atomics::wait_result< T > result = m_wrapper.a.wait_for(m_value1, std::chrono::milliseconds(200));
+
+        test_clock::time_point wakeup_time = test_clock::now();
+
+        if ((wakeup_time - start_time) < std::chrono::milliseconds(200))
+        {
+            std::cout << BOOST_CURRENT_FUNCTION << ": thread woke up too soon: "
+                << std::chrono::duration_cast< std::chrono::milliseconds >(wakeup_time - start_time).count() << " ms" << std::endl;
+            return false;
+        }
+
+        BOOST_TEST_EQ(result.value, m_value1);
+        BOOST_TEST(result.timeout);
+
+        return true;
+    }
+};
+
+template< template< typename > class Wrapper, typename T >
+inline void test_rel_timeout(T value1)
+{
+    for (unsigned int i = 0u; i < test_retry_count; ++i)
+    {
+        // Avoid creating IPC atomics on the stack as this breaks on Darwin
+        std::unique_ptr< rel_timeout_test< Wrapper, T > > test(new rel_timeout_test< Wrapper, T >(value1));
+        if (test->run())
+            return;
+
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    }
+
+    BOOST_ERROR("rel_timeout_test could not complete because blocked thread wake up too soon");
+}
+
+/*!
+ * The test verifies that notifying interrupts a waiting operation with an absolute timeout.
+ */
+template< template< typename > class Wrapper, typename T, typename Clock >
+class notify_abs_timeout_test
+{
+private:
+    struct thread_state
+    {
+        boost::atomics::wait_result< T > m_received_result;
+        typename Clock::time_point m_wakeup_time;
+
+        explicit thread_state(T value)
+        {
+            m_received_result.value = value;
+            m_received_result.timeout = true;
+        }
+    };
+
+private:
+    Wrapper< T > m_wrapper;
+
+    char m_padding[1024];
+
+    T m_value1, m_value2;
+
+    test_barrier m_barrier;
+
+    thread_state m_thread1_state;
+
+public:
+    explicit notify_abs_timeout_test(T value1, T value2) :
+        m_wrapper(value1),
+        m_value1(value1),
+        m_value2(value2),
+        m_barrier(2),
+        m_thread1_state(value1)
+    {
+    }
+
+    bool run()
+    {
+        test_thread thread1([this]() { this->thread_func(&this->m_thread1_state); });
+
+        m_barrier.arrive_and_wait();
+
+        typename Clock::time_point start_time = Clock::now();
+
+        std::this_thread::sleep_until(start_time + std::chrono::milliseconds(200));
+
+        m_wrapper.a.store(m_value2, boost::memory_order_release);
+        m_wrapper.a.notify_all();
+
+        if (!thread1.try_join_for(std::chrono::seconds(5)))
+        {
+            BOOST_ERROR("Thread 1 failed to join");
+            std::abort();
+        }
+
+        if (m_wrapper.a.has_native_wait_notify())
+        {
+            if ((m_thread1_state.m_wakeup_time - start_time) < std::chrono::milliseconds(200))
+            {
+                std::cout << BOOST_CURRENT_FUNCTION << ": first thread woke up too soon: "
+                    << std::chrono::duration_cast< std::chrono::milliseconds >(m_thread1_state.m_wakeup_time - start_time).count() << " ms" << std::endl;
+                return false;
+            }
+        }
+
+        BOOST_TEST_EQ(m_thread1_state.m_received_result.value, m_value2);
+        BOOST_TEST(!m_thread1_state.m_received_result.timeout);
+
+        return true;
+    }
+
+private:
+    void thread_func(thread_state* state)
+    {
+        m_barrier.arrive_and_wait();
+
+        state->m_received_result = m_wrapper.a.wait_until(m_value1, Clock::now() + std::chrono::seconds(2));
+        state->m_wakeup_time = Clock::now();
+    }
+};
+
+template< template< typename > class Wrapper, typename Clock, typename T >
+inline void test_notify_abs_timeout(T value1, T value2)
+{
+    for (unsigned int i = 0u; i < test_retry_count; ++i)
+    {
+        // Avoid creating IPC atomics on the stack as this breaks on Darwin
+        std::unique_ptr< notify_abs_timeout_test< Wrapper, T, Clock > > test(new notify_abs_timeout_test< Wrapper, T, Clock >(value1, value2));
+        if (test->run())
+            return;
+
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    }
+
+    BOOST_ERROR("notify_abs_timeout_test could not complete because blocked thread wake up too soon");
+}
+
+/*!
+ * The test verifies that notifying interrupts a waiting operation with a relative timeout.
+ */
+template< template< typename > class Wrapper, typename T >
+class notify_rel_timeout_test
+{
+private:
+    struct thread_state
+    {
+        boost::atomics::wait_result< T > m_received_result;
+        test_clock::time_point m_wakeup_time;
+
+        explicit thread_state(T value)
+        {
+            m_received_result.value = value;
+            m_received_result.timeout = true;
+        }
+    };
+
+private:
+    Wrapper< T > m_wrapper;
+
+    char m_padding[1024];
+
+    T m_value1, m_value2;
+
+    test_barrier m_barrier;
+
+    thread_state m_thread1_state;
+
+public:
+    explicit notify_rel_timeout_test(T value1, T value2) :
+        m_wrapper(value1),
+        m_value1(value1),
+        m_value2(value2),
+        m_barrier(2),
+        m_thread1_state(value1)
+    {
+    }
+
+    bool run()
+    {
+        test_thread thread1([this]() { this->thread_func(&this->m_thread1_state); });
+
+        m_barrier.arrive_and_wait();
+
+        test_clock::time_point start_time = test_clock::now();
+
+        std::this_thread::sleep_until(start_time + std::chrono::milliseconds(200));
+
+        m_wrapper.a.store(m_value2, boost::memory_order_release);
+        m_wrapper.a.notify_all();
+
+        if (!thread1.try_join_for(std::chrono::seconds(5)))
+        {
+            BOOST_ERROR("Thread 1 failed to join");
+            std::abort();
+        }
+
+        if (m_wrapper.a.has_native_wait_notify())
+        {
+            if ((m_thread1_state.m_wakeup_time - start_time) < std::chrono::milliseconds(200))
+            {
+                std::cout << BOOST_CURRENT_FUNCTION << ": first thread woke up too soon: "
+                    << std::chrono::duration_cast< std::chrono::milliseconds >(m_thread1_state.m_wakeup_time - start_time).count() << " ms" << std::endl;
+                return false;
+            }
+        }
+
+        BOOST_TEST_EQ(m_thread1_state.m_received_result.value, m_value2);
+        BOOST_TEST(!m_thread1_state.m_received_result.timeout);
+
+        return true;
+    }
+
+private:
+    void thread_func(thread_state* state)
+    {
+        m_barrier.arrive_and_wait();
+
+        state->m_received_result = m_wrapper.a.wait_for(m_value1, std::chrono::seconds(2));
+        state->m_wakeup_time = test_clock::now();
+    }
+};
+
+template< template< typename > class Wrapper, typename T >
+inline void test_notify_rel_timeout(T value1, T value2)
+{
+    for (unsigned int i = 0u; i < test_retry_count; ++i)
+    {
+        // Avoid creating IPC atomics on the stack as this breaks on Darwin
+        std::unique_ptr< notify_rel_timeout_test< Wrapper, T > > test(new notify_rel_timeout_test< Wrapper, T >(value1, value2));
+        if (test->run())
+            return;
+
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    }
+
+    BOOST_ERROR("notify_rel_timeout_test could not complete because blocked thread wake up too soon");
 }
 
 //! Invokes all wait/notify tests
@@ -299,6 +639,12 @@ void test_wait_notify_api_impl(T value1, T value2, T value3, std::true_type)
     test_wait_value_mismatch< Wrapper >(value1, value2);
     test_notify_one< Wrapper >(value1, value2, value3);
     test_notify_all< Wrapper >(value1, value2);
+    test_abs_timeout< Wrapper, std::chrono::system_clock >(value1);
+    test_abs_timeout< Wrapper, std::chrono::steady_clock >(value1);
+    test_rel_timeout< Wrapper >(value1);
+    test_notify_abs_timeout< Wrapper, std::chrono::system_clock >(value1, value2);
+    test_notify_abs_timeout< Wrapper, std::chrono::steady_clock >(value1, value2);
+    test_notify_rel_timeout< Wrapper >(value1, value2);
 }
 
 template< template< typename > class Wrapper, typename T >
